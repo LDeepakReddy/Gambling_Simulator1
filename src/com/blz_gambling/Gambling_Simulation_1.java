956f433cdaa1af6803f4574990539b0d4656896f
LDeepakReddy <l.deepakreddy.com>
2022-02-25 03:43:17 +0530

    added the logic to get exit if gets or looses 50%

--- src/com/blz_gambling/Gambling_Simulation_1.java
+++ src/com/blz_gambling/Gambling_Simulation_1.java
@@ -3,18 +3,22 @@ package com.blz_gambling;
 public class Gambling_Simulation_1 {
     public static final int DAY_STARTING_MONEY = 100;
     public static final int BET_PER_EVERYGAME = 1;
+    public static final int EXIT_MARGIN = (DAY_STARTING_MONEY / 100) * 50;
+    public static final int WINNING_MARGIN = DAY_STARTING_MONEY + EXIT_MARGIN;
+    public static final int LOOSING_MARGIN = DAY_STARTING_MONEY - EXIT_MARGIN;
 
     public static void main(String[] args) {
         int totalCash = DAY_STARTING_MONEY;
-        System.out.println("Day starting cash is " + DAY_STARTING_MONEY);
 
-        int game = (int) (Math.random() * 10) % 2; //to check the player is win or lost
-        if (game == 1) {
-            totalCash += BET_PER_EVERYGAME;
-            System.out.println("player won the bet and total cash is " + totalCash);
-        } else {
-            totalCash -= BET_PER_EVERYGAME;
-            System.out.println("Player lost the bet and total cash is " + totalCash);
+        while (totalCash < WINNING_MARGIN && totalCash > LOOSING_MARGIN) {
+            int game = (int) (Math.random() * 10) % 2; //to check the player is win or lost
+            if (game == 1) {
+                totalCash += BET_PER_EVERYGAME;
+                System.out.println("won " + totalCash);
+            } else {
+                totalCash -= BET_PER_EVERYGAME;
+                System.out.println("loss " + totalCash);
+            }
         }
     }
 }
