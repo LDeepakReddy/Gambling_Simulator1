da7703131f51018eed6beba0f332a9d55463f541
LDeepakReddy <l.deepakreddy.com>
2022-02-25 02:33:21 +0530

    starting stake and bet for every game

---
+++ src/com/blz_gambling/Gambling_Simulation_1.java
@@ -0,0 +1,11 @@
+package com.blz_gambling;
+
+public class Gambling_Simulation_1 {
+    public static final int DAY_STARTING_MONEY = 100;
+    public static final int BET_PER_EVERYGAME = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Day Starting money is "+ DAY_STARTING_MONEY+ "$ and Bet per game is "+BET_PER_EVERYGAME+"$" );
+
+    }
+}
