8471fba9099d0cc74193719b2fd0e596f37ec20d
LDeepakReddy <l.deepakreddy.com>
2022-02-25 03:04:18 +0530

    Functionality to make or loose bet

--- src/com/blz_gambling/Gambling_Simulation_1.java
+++ src/com/blz_gambling/Gambling_Simulation_1.java
@@ -5,7 +5,16 @@ public class Gambling_Simulation_1 {
     public static final int BET_PER_EVERYGAME = 1;
 
     public static void main(String[] args) {
-        System.out.println("Day Starting money is " + DAY_STARTING_MONEY + "$ and Bet Per game is " + BET_PER_EVERYGAME + "$");
+        int totalCash = DAY_STARTING_MONEY;
+        System.out.println("Day starting cash is " + DAY_STARTING_MONEY);
 
+        int game = (int) (Math.random() * 10) % 2; //to check the player is win or lost
+        if (game == 1) {
+            totalCash += BET_PER_EVERYGAME;
+            System.out.println("player won the bet and total cash is " + totalCash);
+        } else {
+            totalCash -= BET_PER_EVERYGAME;
+            System.out.println("Player lost the bet and total cash is " + totalCash);
+        }
     }
 }
