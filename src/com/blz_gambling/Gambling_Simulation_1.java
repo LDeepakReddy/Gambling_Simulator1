61ad3cdb705f0fe493dc5bd954719903e916a0f2
LDeepakReddy <l.deepakreddy.com>
2022-02-25 02:38:23 +0530

    Displaying initial amount and bet

--- src/com/blz_gambling/Gambling_Simulation_1.java
+++ src/com/blz_gambling/Gambling_Simulation_1.java
@@ -5,7 +5,7 @@ public class Gambling_Simulation_1 {
     public static final int BET_PER_EVERYGAME = 1;
 
     public static void main(String[] args) {
-        System.out.println("Day Starting money is "+ DAY_STARTING_MONEY+ "$ and Bet per game is "+BET_PER_EVERYGAME+"$" );
+        System.out.println("Day Starting money is " + DAY_STARTING_MONEY + "$ and Bet Per game is " + BET_PER_EVERYGAME + "$");
 
     }
 }
